fb141cec608006b987c796ae7b0c7a369e405f41
Rosa <emiliarosavdh@gmail.com>
2020-09-07 11:44:44 +0200

    still fixing!

--- mainServidorREST.js
+++ mainServidorREST.js
@@ -9,7 +9,7 @@
 // .....................................................................
 const express = require( 'express' )
 const bodyParser = require( 'body-parser' )
-const Logica = require( "./logica/Logica.js" )
+const Logica = require( "./logica/logica.js" )
 const cors = require('cors');
 
 // .....................................................................
